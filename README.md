731fa8dc85d9004c025c091179980f9739e430bb
zakeyah <mom11omar@gmail.com>
2021-05-17 00:09:57 +0300

    still

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # basic-auth
+
+## Author: zakeyah abu yasein
+
+## Links and Resources
+
+
+[heroku](https://zakeyah-basicauth.herokuapp.com/)
+
+[GitHub Repo](https://github.com/zakeyah/basic-auth)
+
+[GitHub Actions](https://github.com/zakeyah/basic-auth/actions)
+
+
+## Notes
+
+- Clone the repo from git hub.
+- Run "npm init -y".
+- Run "npm i express dotenv mongoose base-64 bcrypt cors".
+- Run "npm i -D jest @code-fellows/supergoose".
+
+**.env**
+- MONGODB_URI = 'mongodb://localhost:27017/lab6'
+
+**test**
+- run "npm test"
+
+**run the app**
+- Run "node index.js"
---
+++ src/auth/router.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const express = require('express');
+const bcrypt = require('bcrypt');
+const base64 = require('base-64');
+const Users= require('./models/users-model');
+const basicAuth = require('./middleware/basic')
+
+const router = express.Router();
+
+
+router.post('/signup', async (req, res) => {
+
+    try {
+      req.body.password = await bcrypt.hash(req.body.password, 10);
+      const user = new Users(req.body);
+      const record = await user.save(req.body);
+      res.status(200).json(record);
+    } catch (e) { res.status(403).send("Error Creating User"); }
+  });
+
+
+  router.post('/signin',basicAuth, async (req, res) => {
+
+    /*
+      req.headers.authorization is : "Basic sdkjdsljd="
+      To get username and password from this, take the following steps:
+        - Turn that string into an array by splitting on ' '
+        - Pop off the last value
+        - Decode that encoded string so it returns to user:pass
+        - Split on ':' to turn it into an array
+        - Pull username and password from that array
+    */
+  
+
+    // const user = await Users.findOne({ username: username });
+       res.status(200).json(req.user);
+  
+   
+  
+  });
+
+  module.exports= router;
